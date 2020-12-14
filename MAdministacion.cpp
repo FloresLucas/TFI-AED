f79cc1e471bba3f040750f74a04bcadc42918dbc
Lucas <lucas.flores132@gmail.com>
2020-12-14 04:54:57 -0300

    Funcion Atenciones de Veterinarios cread

--- MAdministacion.cpp
+++ MAdministacion.cpp
@@ -4,6 +4,12 @@
 #include <conio.h>
 
 
+struct fecha
+{
+	int dia;
+	int mes;
+	int anio;
+};
 struct usuarios
 {
     char usuario[11];
@@ -12,26 +18,49 @@ struct usuarios
 };
 struct veterinario
 {
+    char usuario[11];
+    char contrasenia[33];
     char apeynomV[60];
     int matri;
     int dniV;
     char telefo[25];
 };
+struct turnos
+{
+    char apeynomM[100];
+    char apeynomVet[100];
+    int matri;
+    fecha tur;
+    int dniT;
+    char atencion[380];
+};
+struct mascota
+{
+    char apeynom[100];
+    int dni;
+    char localidad[50];
+    fecha edadNaci;
+    int edad;
+    fecha fec;
+    float peso;
+};
 
 
-
-void regVet(FILE *User,usuarios vet);
+void regVet(FILE *Vet,veterinario vet);
 int sonConsecutivos(int prim, int segund);
-int usercheck(FILE *User,char auxuser[11],usuarios vet);
+int usercheck(FILE *Vet,char auxuser[11],veterinario vet);
 int usercheckasi(FILE *User,char auxuser[11],usuarios asist);
 void regAsist(FILE *User,usuarios asist);
-// void mostrarVet(FILE *Vet, veterinario reg);
+void mostrarAtenciones(turnos turno);
+// void mostrarVet(FILE *Vet, veterinario vet);
 
 main()
 {
     FILE *User;
-    usuarios vet;
+    FILE *Vet;
+    veterinario vet;
     usuarios asist;
+    turnos turno;
     
     int opcion;
 	do{
@@ -40,7 +69,7 @@ main()
         printf("\t================================");
 		printf("\n\t1) Registrar Veterinario\n");
 		printf("\t2) Registrar Usuario Asistente\n");
-		printf("\t3) Registro de atenciones por Veterinario\n");
+		printf("\t3) Atenciones por Veterinario\n");
 		printf("\t4) Ranking de Veterinarios por Atenciones\n");
 		printf("\t0) Salir\n");
 		printf("\n\tSeleccione una opcion: ");
@@ -49,9 +78,9 @@ main()
 		
 
 		switch(opcion){
-			
+            
 			case 1:
-				regVet(User,vet);
+				regVet(Vet,vet);
 				printf("\n");
                 system("pause");
 				
@@ -64,7 +93,7 @@ main()
 			break; 
 			case 3:
 				system("cls");
-				// mostrar(User,reg);
+				mostrarAtenciones(turno);
 				printf("\n");
 				system("pause");
 			break; 
@@ -88,14 +117,12 @@ main()
 	while(opcion!=0);
 }
 
-void regVet(FILE *User,usuarios vet)
+void regVet(FILE *Vet,veterinario vet)
 {
     int nn=0,i=0,l=0,p=0,t=0;
     // char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     char temp;
     bool band=false;
-    FILE *Vet;
-    veterinario reg;
     do{
         system("cls");
         printf("Ingrese un nuevo nombre de usuario: ");
@@ -207,7 +234,7 @@ void regVet(FILE *User,usuarios vet)
                     char auxuser[11];
                     strcpy(auxuser,vet.usuario);
 
-                    h=usercheck( User, auxuser, vet);
+                    h=usercheck(Vet, auxuser, vet);
                     if (h==1)
                     {
                         system("cls");
@@ -283,141 +310,151 @@ void regVet(FILE *User,usuarios vet)
                     case '9': f++; break;
                     case '0': f++; break;
                     default: f=0;  break;
-                    if(f>=3)
-                    {
-                        k=1;
-                    }	
+                   	
+                }
+                if(f>=3)
+                {
+                    k=1;
                 }
             }
-            if(k==1)
+            if(f==0)
             {
                 system("cls");
                 printf("La contrasenia es incorrecta\n");
                 printf("\n");
-                system("pause");  
+                system("pause");
             }
             else
             {
-                
-                for(i=0;i<nn;i++)
-                {
-                    switch(vet.contrasenia[i])
-                    {
-                        case '1': o++; break;
-                        case '2': o++; break;
-                        case '3': o++; break;
-                        case '4': o++; break;
-                        case '5': o++; break;
-                        case '6': o++; break;
-                        case '7': o++; break;
-                        case '8': o++; break;
-                        case '9': o++; break;
-                        case '0': o++; break;
-                        case 'A': o++; break;
-                        case 'E': o++; break;
-                        case 'I': o++; break;
-                        case 'O': o++; break;
-                        case 'U': o++; break;
-                        case 'B': o++; break;
-                        case 'C': o++; break;
-                        case 'D': o++; break;
-                        case 'F': o++; break;
-                        case 'G': o++; break;
-                        case 'H': o++; break;
-                        case 'J': o++; break;
-                        case 'K': o++; break;
-                        case 'L': o++; break;
-                        case 'M': o++; break;
-                        case 'N': o++; break;
-                        case 'P': o++; break;
-                        case 'Q': o++; break;
-                        case 'R': o++; break;
-                        case 'S': o++; break;
-                        case 'T': o++; break;
-                        case 'V': o++; break;
-                        case 'W': o++; break;
-                        case 'X': o++; break;
-                        case 'Y': o++; break;
-                        case 'Z': o++; break;
-                        case 'a': o++; break;
-                        case 'e': o++; break;
-                        case 'i': o++; break;
-                        case 'o': o++; break;
-                        case 'u': o++; break;
-                        case 'b': o++; break;
-                        case 'c': o++; break;
-                        case 'd': o++; break;
-                        case 'f': o++; break;
-                        case 'g': o++; break;
-                        case 'h': o++; break;
-                        case 'j': o++; break;
-                        case 'k': o++; break;
-                        case 'l': o++; break;
-                        case 'm': o++; break;
-                        case 'n': o++; break;
-                        case 'p': o++; break;
-                        case 'q': o++; break;
-                        case 'r': o++; break;
-                        case 's': o++; break;
-                        case 't': o++; break;
-                        case 'v': o++; break;
-                        case 'w': o++; break;
-                        case 'x': o++; break;
-                        case 'y': o++; break;
-                        case 'z': o++; break;
-                        default: v=1;  break;
-                    }
-                }
-                if (v==1)
+                if(k==1)
                 {
                     system("cls");
                     printf("La contrasenia es incorrecta\n");
                     printf("\n");
-                    system("pause");
+                    system("pause");  
                 }
                 else
                 {
-                    if (o>=3)
+                    
+                    for(i=0;i<nn;i++)
                     {
-                        for(i=0;i<nn;i++)
+                        switch(vet.contrasenia[i])
                         {
-                            int prim = vet.contrasenia[i];
-                            int segund = vet.contrasenia[i+1];
-                            int dir=sonConsecutivos(prim,segund);
-                            if (dir==1)
-                            {
-                                z++;
-                            }
-                            
+                            case '1': o++; break;
+                            case '2': o++; break;
+                            case '3': o++; break;
+                            case '4': o++; break;
+                            case '5': o++; break;
+                            case '6': o++; break;
+                            case '7': o++; break;
+                            case '8': o++; break;
+                            case '9': o++; break;
+                            case '0': o++; break;
+                            case 'A': o++; break;
+                            case 'E': o++; break;
+                            case 'I': o++; break;
+                            case 'O': o++; break;
+                            case 'U': o++; break;
+                            case 'B': o++; break;
+                            case 'C': o++; break;
+                            case 'D': o++; break;
+                            case 'F': o++; break;
+                            case 'G': o++; break;
+                            case 'H': o++; break;
+                            case 'J': o++; break;
+                            case 'K': o++; break;
+                            case 'L': o++; break;
+                            case 'M': o++; break;
+                            case 'N': o++; break;
+                            case 'P': o++; break;
+                            case 'Q': o++; break;
+                            case 'R': o++; break;
+                            case 'S': o++; break;
+                            case 'T': o++; break;
+                            case 'V': o++; break;
+                            case 'W': o++; break;
+                            case 'X': o++; break;
+                            case 'Y': o++; break;
+                            case 'Z': o++; break;
+                            case 'a': o++; break;
+                            case 'e': o++; break;
+                            case 'i': o++; break;
+                            case 'o': o++; break;
+                            case 'u': o++; break;
+                            case 'b': o++; break;
+                            case 'c': o++; break;
+                            case 'd': o++; break;
+                            case 'f': o++; break;
+                            case 'g': o++; break;
+                            case 'h': o++; break;
+                            case 'j': o++; break;
+                            case 'k': o++; break;
+                            case 'l': o++; break;
+                            case 'm': o++; break;
+                            case 'n': o++; break;
+                            case 'p': o++; break;
+                            case 'q': o++; break;
+                            case 'r': o++; break;
+                            case 's': o++; break;
+                            case 't': o++; break;
+                            case 'v': o++; break;
+                            case 'w': o++; break;
+                            case 'x': o++; break;
+                            case 'y': o++; break;
+                            case 'z': o++; break;
+                            default: v=1;  break;
                         }
-                        if (z>0)
-                        {
-                            system("cls");
-                            printf("La contrasenia es incorrecta,1\n");
-                            printf("\n");
-                            system("pause");
-                        }
-                        else
-                        {
-                            system("cls");
-                            printf("Nueva contrasenia registrada correctamente\n");
-                            printf("\n");
-                            band1=true;
-                        } 
-                        
                     }
-                    else
+                    if (v==1)
                     {
                         system("cls");
                         printf("La contrasenia es incorrecta\n");
                         printf("\n");
                         system("pause");
                     }
-                    
-                    
+                    else
+                    {
+                        if (o>=3)
+                        {
+                            for(i=0;i<nn;i++)
+                            {
+                                int prim = vet.contrasenia[i];
+                                int segund = vet.contrasenia[i+1];
+                                int dir=sonConsecutivos(prim,segund);
+                                if (dir==1)
+                                {
+                                    z++;
+                                }
+                                
+                            }
+                            if (z>0)
+                            {
+                                system("cls");
+                                printf("La contrasenia es incorrecta,1\n");
+                                printf("\n");
+                                system("pause");
+                            }
+                            else
+                            {
+                                system("cls");
+                                printf("Nueva contrasenia registrada correctamente\n");
+                                printf("\n");
+                                band1=true;
+                            } 
+                            
+                        }
+                        else
+                        {
+                            system("cls");
+                            printf("La contrasenia es incorrecta\n");
+                            printf("\n");
+                            system("pause");
+                        }
+                        
+                        
+                    }
                 }
-                
-            }
+            }  
         }
         else
         {
@@ -429,45 +466,43 @@ void regVet(FILE *User,usuarios vet)
 
     }while(!band1);
     
-    User=fopen("Usuarios.dat", "a+b");
+    
+    Vet=fopen("Veterinarios.dat", "a+b");
 
     vet.usuario;
     vet.contrasenia;
-    
-    fwrite(&vet,sizeof(usuarios),1,User);
 
-
-    printf("\nUsuario registrado correctamente");
+    printf("\nUsuario veterinario registrado correctamente");
     printf("\n\n");
     system("pause");
     system("cls");
 
-    Vet=fopen("Veterinarios.dat","a+b");
     
     printf("Ingrese los datos del Veterinario\n");
 
+
     printf("\nIngrese Apellido y Nombre: ");
     _flushall();
-    gets(reg.apeynomV);
+    gets(vet.apeynomV);
     _flushall();
 
     printf("\nIngrese la matricula: ");
-    scanf("%d",&reg.matri);
+    scanf("%d",&vet.matri);
 
     printf("\nIngrese el D.N.I: ");
-    scanf("%d",&reg.dniV);
+    scanf("%d",&vet.dniV);
 
     printf("\nIngrese el telefono: ");
     _flushall();
-   gets(reg.telefo);
+    gets(vet.telefo);
     _flushall();
 
 
-    fwrite(&reg,sizeof(veterinario),1,Vet);
+    fwrite(&vet,sizeof(veterinario),1,Vet);
 
     fclose(Vet);
     
-    // mostrarVet(Vet,reg);
+    // mostrarVet(Vet,vet);
     printf("\n");
     
 
@@ -829,25 +864,11 @@ void regAsist(FILE *User,usuarios asist)
     
     fwrite(&asist,sizeof(usuarios),1,User);
 
-    // rewind(User);
-    // fread(&asist,sizeof(usuarios),1,User);
-    // while (!feof(User)) 
-    // {
-    //     printf("Usuario: %s\n",asist.usuario);
-    //     printf("Contrasenia: %s\n",asist.contrasenia);
-    //     printf("\n");
-    //     fread(&asist,sizeof(usuarios),1,User);
-    // }
     system("cls");
     printf("Usuario registrado correctamente");
     printf("\n");
     fclose(User);  
 }
-
-
-
-
-
 
      
 int sonConsecutivos(int prim, int segund)
@@ -863,22 +884,22 @@ int sonConsecutivos(int prim, int segund)
     }
 }
 
-int usercheck(FILE *User,char auxuser[11],usuarios vet)                   
+int usercheck(FILE *Vet,char auxuser[11],veterinario vet)                   
 {   
-    User=fopen("Usuarios.dat", "a+b");
+    Vet=fopen("Vetererinario", "a+b");
     int h; 
-    rewind(User);
-    fread(&vet,sizeof(usuarios),1,User);
-    while (!feof(User)) 
+    rewind(Vet);
+    fread(&vet,sizeof(veterinario),1,Vet);
+    while (!feof(Vet)) 
     {
         if (strcmp(auxuser,vet.usuario)==0)
         {
             h=1;    
         }
-        fread(&vet,sizeof(usuarios),1,User);
+        fread(&vet,sizeof(veterinario),1,Vet);
     }
      
-    fclose(User);
+    fclose(Vet);
 
     return h;
 } 
@@ -903,7 +924,42 @@ int usercheckasi(FILE *User,char auxuser[11],usuarios asist)
     return h;
 }
 
-// void mostrarVet(FILE *Vet, veterinario reg)
+void mostrarAtenciones(turnos turno)
+{
+   FILE *Atent;
+   
+    Atent=fopen("Atenciones.dat","rb");
+
+     if(Atent==NULL)
+    {
+        system("CLS");
+        printf("\n\nNo existe el archivo todavia\n");
+        printf("\nRegistrar previamente Atenciones Modulo de Consultorio\n");
+        printf("\n\n\t");
+
+    }
+    else
+    {
+        system("cls");
+        rewind(Atent);
+        fread(&turno,sizeof(turnos),1,Atent);
+        while (!feof(Atent))
+        {
+            printf("\tApellido y Nombre de la Mascota: %s\n",turno.apeynomM);
+            printf("\tD.N.I: %d\n",turno.dniT);
+            printf("\tApellido y Nombre del Veterinario: %s\n",turno.apeynomVet);
+            printf("\tMatricula: %d\n",turno.matri);
+            printf("\tFecha: %d/%d/%d\n",turno.tur.dia,turno.tur.mes,turno.tur.anio);
+            printf("\tAtencion:\n\t%s",turno.atencion);
+            printf("\n\n");
+            fread(&turno,sizeof(turnos),1,Atent);
+        }
+
+        fclose(Atent);
+    }
+}
+
+// void mostrarVet(FILE *Vet, veterinario vet)
 // {
 //     Vet=fopen("Veterinarios.dat","rb");
     
@@ -919,12 +975,12 @@ int usercheckasi(FILE *User,char auxuser[11],usuarios asist)
 //     {
 //         rewind(Vet);
 
-//     printf("Apellido y Nombre \t  Matricula  \t D.N.I  \t Telefono\n");
-//     fread(&reg,sizeof(veterinario),1,Vet);
+//         printf("Usuario \t Contraseña \tApellido y Nombre \t  Matricula  \t D.N.I  \t Telefono\n");
+//         fread(&vet,sizeof(veterinario),1,Vet);
 //         while (!feof(Vet))
 //         {
-//             printf("\n%s\t%d\t%d\t%s\n",reg.apeynomV,reg.matri,reg.dniV,reg.telefo);
-//             fread(&reg,sizeof(veterinario),1,Vet);
+//             printf("\n%s\t%s\t%s\t%d\t%d\t%s\n",vet.usuario,vet.contrasenia,vet.apeynomV,vet.matri,vet.dniV,vet.telefo);
+//             fread(&vet,sizeof(veterinario),1,Vet);
 
 //         }
 
